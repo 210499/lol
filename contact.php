2b501814320cf3079b6b5dfb0c9e3dd022282965
210499 <210499@hkis.edu.hk>
2016-02-26 10:15:26 +0800

    Created Website
    TESTING

--- contact.php
+++ contact.php
@@ -1,33 +1,22 @@
-    <?php 
-     $to ='210499@hkis.edu.hk';
-     $from = 'santoshdevnath15@gmail.com';
-     $subject = 'Html Eamil Check';
-     $message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-                <html xmlns="http://www.w3.org/1999/xhtml">
-                <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>Untitled Document</title>
-                </head>
+<?php
 
-                <body>
-                <div style="width:400px; height:400px; background-color:#096; color:#FFF">
-                <h1>Welcome to inet shopping site</h1>
-                <p>
-                Dear Santosh,
-                We have received your Cash-On-Delivery order. We need to confirm your order by call. We will get in touch with you soon on Your order               will be processed only after confirmation.
-                For the payment, please pay Rs. 4000 cash to the courier person when he delivers the product.
-                "Please call us on 8882248819 to confirm after you deposit the amount."
-                </div>
-                </body>
-                </html>';
-    $headers = "From: $from\n";
-    $headers .= "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-    if(mail($to, $subject, $message, $headers)){
-        echo 'ok';
-    }
-    else{
-        echo 'error';
-    }
+if(isset($_POST['send'])) {
+   // Prepare the email
+$to = '210499@hkis.edu.hk';
 
+$name = $_POST['name'];
+$mail_from = $_POST['email'];
+   $subject = 'Message sent from website';
+   $message = $_POST['message'];
+
+$header = "From: $name <$mail_from>";
+
+   // Send it
+   $sent = mail($to, $subject, $message, $header);
+   if($sent) {
+   echo 'Your message has been sent successfully!';
+   } else {
+   echo 'Sorry, your message could not send.';
+   }
+}
 ?>
